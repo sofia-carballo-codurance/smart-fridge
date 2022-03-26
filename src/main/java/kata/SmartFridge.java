27fa9b608b6f0b0a8362cf09967380a9ac462e78
Sofia Carballo <sofia.carballo@codurance.com>
2022-03-26 18:26:19 +0100

    Add acceptance test

---
+++ src/main/java/kata/SmartFridge.java
@@ -0,0 +1,27 @@
+package kata;
+
+public class SmartFridge {
+    public void signalFridgeDoorOpened() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void scanAddedItem(String name, String date, String opened) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void signalFridgeDoorClosed() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void simulateDayOver() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void scanRemovedItem(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String showDisplay() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/test/java/kata/AcceptanceShould.java
@@ -0,0 +1,52 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AcceptanceShould {
+
+    @Test void
+    display_fridge_items() {
+        String expectedOut = """
+            EXPIRED: Milk
+            Lettuce: 0 days remaining
+            Peppers: 1 day remaining
+            Cheese: 31 days remaining
+            """;
+
+        var smartFridge = new SmartFridge();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.scanAddedItem("Milk", "21/10/21", "sealed");
+        smartFridge.scanAddedItem("Cheese", "18/11/21", "sealed");
+        smartFridge.scanAddedItem("Beef", "20/10/21", "sealed");
+        smartFridge.scanAddedItem("Lettuce", "22/10/21", "sealed");
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.simulateDayOver();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.scanRemovedItem("Milk");
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.scanAddedItem("Milk", "26/10/21", "opened");
+        smartFridge.scanAddedItem("Peppers", "23/10/21", "opened");
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.simulateDayOver();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.scanRemovedItem("Beef");
+        smartFridge.scanRemovedItem("Lettuce");
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.scanAddedItem("Lettuce", "22/10/21", "opened");
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.signalFridgeDoorOpened();
+        smartFridge.signalFridgeDoorClosed();
+        smartFridge.simulateDayOver();
+
+        String result = smartFridge.showDisplay();
+
+        assertEquals(expectedOut, result);
+    }
+}
